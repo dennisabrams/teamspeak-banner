dd1061039451ab4ff74f1f333f96c9f29750d8f1
Dennis Abrams <contact@dennis-abrams.com>
2023-03-10 19:45:33 +0100

    Update config.php

--- config.php
+++ config.php
@@ -13,7 +13,7 @@ $ts3_libary = "../vendor/planetteamspeak/ts3-php-framework/libraries/TeamSpeak3/
 
 // Serverquery connection
 $serverquery_username = "serveradmin"; // Default serverquery_username: "serveradmin"
-$serverquery_password = "PASSWORD";
+$serverquery_password = ""; // Password
 $server_ip = "127.0.0.1"; // Set to "127.0.0.1" if the webserver and TS3 server are hosted on the same server (localhost)
 $serverquery_port = "10011"; // Default serverquery_port: "10011"
 $server_port = "9987"; // Default server_port: "9987"
@@ -25,28 +25,24 @@ $ts3_bots = 0; // Amount of Bots on the TS3 server
 date_default_timezone_set("Europe/Berlin"); // Locale date information
 setlocale(LC_ALL, "de_DE.utf8"); // Locale date spelling
 
-// Background Image
-$background = "images/background.png"; // Background Image Path
+// Background image
+$background = "images/background.png"; // Background image path (3:1 aspect ratio)
 
-// Text Config
-$font = "fonts/ReadexPro.ttf"; // Font style Path
-$text_large = 130; // Size of the current time
+// Text config
+$font = "fonts/ReadexPro.ttf"; // Font style path
+$text_time = 130; // Size of the current time
+$text_server = 100; // Size of the server name
 $text_normal = 50; // Size of the headlines
 $text_small = 40; // Size of informations
-$text_color1 = array("R" => 255, "G" => 255, "B" => 255); // Custom text color RGB (0 - 255)
-$text_color2 = array("R" => 105, "G" => 108, "B" => 124); // Custom text color RGB (0 - 255)
+$text_color1 = array("R" => 255, "G" => 255, "B" => 255, "A" => 0); // Custom text color RGBA (Red, Green, Blue, Alpha)
+$text_color2 = array("R" => 105, "G" => 108, "B" => 124, "A" => 0); // Custom text color RGBA (Red, Green, Blue, Alpha)
 
-// Logo Image (If  there is no Logo it will just show the name of the server)
-$logo = ""; // Logo Image Path for example: images/logo.png
-$logo_size = 50; // Logo size in percentage
-$logo_transparency = 100; // Logo Transparency (0 - 100)
-$lpl = "30"; // Distance left (0 - 100)
-$lpt = "30"; // Distance top (0 - 100)
+// Box color
+$box = array("R" => 0, "G" => 0, "B" => 0, "A" => 70); // Custom box color RGBA (Red, Green, Blue, Alpha)
 
-// Server Name
-$sts = "100"; // Server text size
-$spl = "36"; // Distance left (0 - 100)
-$spt = "53"; // Distance top (0 - 100)
+// Logo image (if  there is no logo it will just shows the name of the server)
+$logo = ""; // Logo image path
+$logo_size = 100; // Logo size
 
 // Custom Text
 $custom = ""; // Custom text (\n = new line)
@@ -55,25 +51,29 @@ $itr = ""; // Custom text rotation (0 - 360)
 $ipl = ""; // Distance left (0 - 100)
 $ipt = ""; // Distance top (0 - 100)
 
-// Server Version
+// Server version
 $version = 1; // 1 = Enabled, 0 = Disabled
 
-// Server Group Icons
+// Server group icons (more info: server_groups/README.md)
 $groups = 0; // 1 = Enabled, 0 = Disabled
-$max_groups = 5; // Maximum of Server Group Icons that will be shown
+$max_groups = 5; // Maximum of server group icons that will be shown
 $gpl = 9; // Distance left (0 - 100)
+$bad_groups = array(); // Icon ID's you don't want to show e.g. if they are invisible: array(ID, ID, ...)
 
 // Translation
-$t1 = "Bandwith (last min.)"; // Bandwith (last min.)
-$t2 = "Connection Information"; // Connection Information
-$t3 = "Upload (Bytes/s)"; // Upload (Bytes/s)
-$t4 = "Download (Bytes/s)"; // Download (Bytes/s)
-$t5 = "Nickname"; // Nickname
-$t6 = "Connected since"; // Connected since
-$t7 = "Total Connections"; // Total Connections
-$t8 = "Groups"; // Groups
-$t9 = "Clients"; // Clients
-$t10 = "Channels"; // Channels
-$t11 = "Uptime" // Uptime
+$t1 = "Bandwith (last min.)"; // "Bandwith (last min.)"
+$t2 = "Connection information"; // "Connection information"
+$t3 = "Upload (Bytes/s)"; // "Upload (Bytes/s)"
+$t4 = "Download (Bytes/s)"; // "Download (Bytes/s)"
+$t5 = "Nickname"; // "Nickname"
+$t6 = "Connected since"; // "Connected since"
+$t7 = "Total connections"; // "Total connections"
+$t8 = "Groups"; // "Groups"
+$t9 = "Clients"; // "Clients"
+$t10 = "Channels"; // "Channels"
+$t11 = "Workload"; // "Workload"
+$t12 = "Uptime"; // "Uptime"
+$t13 = "No client info"; // "No client info"
+$t14 = "Check if you are connected \nto a VPN or proxy server and \nreconnect to"; // "Check if you are connected \nto a VPN or proxy server and \nreconnect to"
 
 ?>
